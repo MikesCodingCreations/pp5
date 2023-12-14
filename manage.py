d6a72d19d925bd0ce8135bdefba9c1e0b378394d
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-12-14 00:06:42 +0000

    Allauth setup

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pure_protein_powder.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pure_protein_powders.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- pure_protein_powders/asgi.py
+++ pure_protein_powders/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pure_protein_powder project.
+ASGI config for pure_protein_powders project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pure_protein_powder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pure_protein_powders.settings')
 
 application = get_asgi_application()

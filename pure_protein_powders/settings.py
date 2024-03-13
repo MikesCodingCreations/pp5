e3a681c841ae972453e9167b093f4abefd01f9e9
Mikes Coding Creations <mikescodingcreations@gmail.com>
2024-03-13 14:45:25 +0000

    attempt one to resolve server error 500

--- pure_protein_powders/settings.py
+++ pure_protein_powders/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'eg-gl7t*qz=lo5he)24lay$pis1ck*7&p08-^l^lv*h@uicpe!')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

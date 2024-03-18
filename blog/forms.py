4c84ad3394b6b8e5fcec94f7abd65f9637ecf441
Mikes Coding Creations <mikescodingcreations@gmail.com>
2024-03-18 13:51:50 +0000

    Blog comment functionality implemented

--- blog/forms.py
+++ blog/forms.py
@@ -4,13 +4,4 @@ from .models import Comment, Post
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = [
-            'content'
-        ]
-
-class BlogForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = "__all__"
-        exclude = ['author', 'created']
+        fields = ('body',)

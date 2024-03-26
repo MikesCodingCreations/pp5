4188e818dbb4c96130240a27b897a1aaa73b9f45
Mikes Coding Creations <mikescodingcreations@gmail.com>
2024-03-26 12:22:10 +0000

    Add to wishlist and remove from wishlist now working

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,20 +6,22 @@ from wishlist.models import WishItem
 
 
 def view_wishlist(request):
-    wish_items = WishItem.objects.filter(user=request.user)
-    context = {
-        'wish_items': wish_items,
-    }
+  wish_items = WishItem.objects.filter(user=request.user)
+  context = {
+      'wish_items': wish_items,
+      'has_wishlist_items': wish_items.exists(),  # Check if any items exist
+  }
 
-    return render(request, 'wishlist.html', context)
+  return render(request, 'wishlist.html', context)
+
 
 def add_to_wishlist(request, item_id):
   product = get_object_or_404(Product, pk=item_id)
   size = request.POST.get('product_size', None)
 
   if not request.user.is_authenticated:
-      messages.warning(request, 'You need to be logged in to add to wishlist')
-      return redirect('login')
+    messages.warning(request, 'You need to be logged in to add to wishlist')
+    return redirect('login')
 
   WishItem.objects.create(user=request.user, product=product, size=size)
   messages.success(request, f'Added {product.name} to your wishlist')
@@ -28,26 +30,17 @@ def add_to_wishlist(request, item_id):
 
 
 def remove_from_wishlist(request, item_id):
+  try:
+    wish_item = WishItem.objects.get(user=request.user, product__pk=item_id)
 
-    try:
-        product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        wishlist = request.session.get('wishlist', {})
+    wish_item.delete()
+    messages.success(request, f'Removed {wish_item.product.name} from your wishlist')
+    return redirect('view_wishlist')
 
-        if size:
-            del wishlist[item_id]['items_by_size'][size]
-            if not wishlist[item_id]['items_by_size']:
-                wishlist.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your wishlist')
-        else:
-            wishlist.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your wishlist')
+  except WishItem.DoesNotExist:
+    messages.warning(request, 'Item not found in your wishlist')
+    return redirect('view_wishlist')
 
-        request.session['wishlist'] = wishlist
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+  except Exception as e:
+    messages.error(request, f'Error removing item: {e}')
+    return HttpResponse(status=500)
